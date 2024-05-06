b28830403058a10f8733a56c43f1cfdde0b7cac5
Devsi <grace.emily876@gmail.com>
2024-05-06 19:21:28 +0530

    login details

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -282,30 +282,83 @@ const Navbar = () => {
 
   return (
     <div>
-      {wallet ? (
-          <div className="flex gap-4">
-          <Link href="/profile">{avatarUrl && <img src={avatarUrl} alt="Avatar" style={{width: 40}}/>} </Link>
-          <div>
-          <div className="rounded-lg text-sm font-semibold text-center">
-            {wallet.slice(0, 4)}...{wallet.slice(-4)}
-          </div>
-          <button
-            onClick={handleDeleteCookie}
-            style={logout}
-            className="mx-auto hover:text-red-400 text-black text-sm font-semibold"
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
-          >
-            Logout
-          </button>
-          </div>
-          </div>
+
+<div className="flex">
+{userAddress ? (
+            <div className='flex gap-2'>
+              {avatarUrl && <img src={avatarUrl} alt="Avatar" style={{width: 40}}/>}
+              <dd className='text-sm leading-6 text-gray-700 sm:col-span-2 text-white'>
+                <div>{userAddress?.slice(0, 3)}...{userAddress?.slice(-3)}</div>
+                <div className="flex justify-center">
+                  <button
+                    type='button'
+                    className='rounded-md bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
+                    onClick={() => {
+                      navigator.clipboard.writeText(userAddress);
+                    }}
+                  >
+                    Copy
+                  </button>
+                </div>
+              </dd>
+            </div>
+          ) : null}
+          {userAddress ? (
+            <div className=''>
+              <dd className='mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 text-white'>
+                <div className="justify-end flex">{userBalance.toFixed(4)} SUI</div>
+                <div>
+                  <button
+                    type='button'
+                    className='rounded-md bg-white py-1 px-2 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
+                    disabled={!userAddress}
+                    onClick={() => giveSomeTestCoins(userAddress)}
+                  >
+                    Get Testnet Coins
+                  </button>
+                </div>
+              </dd>
+            </div>
+          ) : null}
+
+</div>
+
+      {userAddress ? (
+          <></>
       )
     :(
-      <button onClick={()=>{setloginbox(true)}} className="px-4">Login</button>
+      // <button onClick={()=>{setloginbox(true)}} className="px-4">Login</button>
+      <button onClick={redirectlogin} className='bg-white text-gray-700 hover:text-gray-900 font-semibold py-2 px-2 border rounded-lg flex items-center'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  x='0px'
+                  y='0px'
+                  width='24'
+                  height='24'
+                  viewBox='0 0 48 48'
+                >
+                  <path
+                    fill='#FFC107'
+                    d='M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z'
+                  ></path>
+                  <path
+                    fill='#FF3D00'
+                    d='M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z'
+                  ></path>
+                  <path
+                    fill='#4CAF50'
+                    d='M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z'
+                  ></path>
+                  <path
+                    fill='#1976D2'
+                    d='M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z'
+                  ></path>
+                </svg>
+                <span>Login with Google</span>
+              </button>
     )}
 
-{ loginbox && (<animated.div style={modalProps} className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+{/* { loginbox && (<animated.div style={modalProps} className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-16 rounded-lg flex gap-y-6 justify-center w-[30rem] items-center flex-col text-center relative">
         <h2 className="text-2xl font-bold mb-4">Login Options</h2>
 
@@ -338,7 +391,7 @@ const Navbar = () => {
                 <span>Login with Google</span>
               </button>
       </div>
-    </animated.div>)}
+    </animated.div>)} */}
     </div>
   );
 };
