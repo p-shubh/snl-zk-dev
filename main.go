7c17000aefd9e3302bb9d98df83e5e06cb6b3fea
Suraj Mahto <surajmahto255@gmail.com>
2024-06-03 01:29:08 +0530

    faucet fixed

--- main.go
+++ main.go
@@ -1,22 +1,75 @@
-package main
+package router
 
 import (
-	"log"
+	"net/http"
 	"os"
-	router "snl/Router"
-	dbflow "snl/dbFlow"
+	suiSlnRouter "snl/sui_snl/ApiRouting"
 
-	"github.com/joho/godotenv"
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	if os.Getenv("LOAD_CONFIG_FILE") == "" {
-		// Load environment variables from .env file
-		err := godotenv.Load()
-		if err != nil {
-			log.Printf("Error in reading the config file : %v\n", err)
-		}
+var (
+	ginApp = gin.Default()
+)
+
+func CORS(c *gin.Context) {
+
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Headers", "*")
+
+	if c.Request.Method != "OPTIONS" {
+		c.Next()
+	} else {
+		c.AbortWithStatus(http.StatusOK)
 	}
-	dbflow.DbTest()
-	router.HandleRequest()
+}
+func HealthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": 200, "message": "Server is UP and running",
+	})
+}
+func HandleRequest() {
+	if os.Getenv("APP_MODE_DEBUG") == "" {
+		gin.SetMode(gin.ReleaseMode)
+	} else {
+		gin.SetMode(gin.DebugMode)
+	}
+	ginApp.Use(CORSMiddleware())
+
+	ginApp.Use(static.Serve("/", static.LocalFile("./web", false)))
+
+	ginApp.GET("/healthcheck", HealthCheck)
+
+	// no route redirect to frontend app
+	ginApp.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"status": 404, "message": "Invalid Endpoint Request"})
+	})
+	ApplyRoutes(&ginApp.RouterGroup)
+
+	ginApp.Run(":6060")
+}
+
+func ApplyRoutes(r *gin.RouterGroup) {
+	v1 := r.Group("/v1.0")
+	{
+		suiSlnRouter.SuiSlnApplyRoutes(v1)
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
--- sui_snl/DBoperation/sln.sui.db.operation.go
+++ sui_snl/DBoperation/sln.sui.db.operation.go
@@ -2,7 +2,6 @@ package sln_sui_dboperation
 
 import (
 	"net/http"
-	"os/exec"
 	dbflow "snl/dbFlow"
 	"snl/model"
 	gamecreatedeploy "snl/sui_snl/smartContractDeploy/gameCreateDeploy"
@@ -21,10 +20,7 @@ func CreateSlnSui(c *gin.Context) {
 	}
 	newSlnSui.CreatedAt = time.Now()
 	newSlnSui.ContractAddress = gamecreatedeploy.GameCreateDeploy()
-	if len(newSlnSui.ContractAddress) == 0 {
-		exec.Command("sui", "client", "faucet")
-		newSlnSui.ContractAddress = gamecreatedeploy.GameCreateDeploy()
-	}
+
 	// Save the new record to the database
 	if err := db.Create(&newSlnSui).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
